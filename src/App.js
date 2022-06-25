da5faecd80896844bc8cc9756ea939993b553bc1
Charles Vidro <cjvidro@mtu.edu>
2022-06-24 20:32:53 -0400

    YT player

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import './App.css';
 import Box from './components/Box';
-import {OrbitControls, Stats} from '@react-three/drei';
-import {Canvas} from '@react-three/fiber';
-import React from "react";
-import Cinematic from "./components/Cinematic";
+import {Html, OrbitControls, Stats} from '@react-three/drei';
+import {Canvas, useFrame} from '@react-three/fiber';
+import React, {useRef} from "react";
 
-function App() {
+function YTVideo(props) {
+  const ref = useRef();
 
   return (
+    <group ref={ref} {...props}>
+      <Html transform position={[0, 5, 0]}>
+        <iframe
+          src={props.link}
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          width="420"
+          height="315"
+        >
+        </iframe>
+      </Html>
+    </group>
+  );
+
+}
+
+function App() {
+  return (
     <>
-      <Cinematic/>
-      <Canvas id="canvas" aspect={2.35}>
-        <color attach="background" args={["black"]}/>
-        <ambientLight/>
-        <pointLight position={[10, 10, 10]}/>
-        <OrbitControls/>
-        <Box position={[-1.2, 0, 0]}/>
-        <Box position={[1.2, 0, 0]}/>
-        <Stats/>
-      </Canvas>
+    <Canvas>
+      <ambientLight intensity={1} color="white"/>
+      <directionalLight intensity={1} color="white"/>
+      <OrbitControls />
+      <Box position={[-8, 0, 0]} />
+      <Stats />
+      <YTVideo link="https://www.youtube.com/embed/7SsrVGHJvaA" position={[-4, 5, 0]}/>
+      <YTVideo link="https://www.youtube.com/embed/XKWcAsT8Sqc" position={[4, 5, 7]} rotation={[0, -1.2, 0]}/>
+    </Canvas>
     </>
   );
 }
